6eb5bc2ad6edd1c6a82fe8fb2116a847c1b4a340
Maria <maria20.ahmad02@gmail.com>
2025-04-27 13:00:49 +0300

    json task

---
+++ merge_json_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# download the json files using wget, -q for quiet mode in order not to output anything in the terminal, and -O for the output file name
+download_files() {
+  wget -q -O tmp_female.json https://assets.harridev.com/interview/oscar_age_female.json
+  wget -q -O tmp_male.json https://assets.harridev.com/interview/oscar_age_male.json
+}
+
+# add gender field directly using map function where key is Gender and Value is F or M based on the file name
+add_gender_fields() {
+  jq 'map(.Gender = "F")' tmp_female.json > tmp_female_with_gender.json
+  jq 'map(.Gender = "M")' tmp_male.json > tmp_male_with_gender.json
+}
+
+# merge the 2 files based on the year and gender
+merge_and_sort() {
+  jq -s 'add | sort_by(.Year, .Gender)' tmp_female_with_gender.json tmp_male_with_gender.json > tmp_merged_sorted.json
+}
+
+# edit the index on the merged file and update the index
+edit_index() {
+  jq 'to_entries | map(.value.Index = (.key + 1) | .value)' tmp_merged_sorted.json > oscar_age_gender.json
+}
+
+# remove temp files
+remove_files() {
+  rm -f tmp_female.json tmp_male.json tmp_female_with_gender.json tmp_male_with_gender.json tmp_merged_sorted.json
+}
+
+download_files
+add_gender_fields
+merge_and_sort
+edit_index
+remove_files
+
